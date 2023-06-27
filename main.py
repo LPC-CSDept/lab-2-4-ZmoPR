352390c1c43c7b843dbf0005f2a5dc42292b5000
Zach Montez <zmontez101200@gmail.com>
2023-06-27 16:02:32 -0700

    commit

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
+    
+    original_str = "Python Programming"
+    sub2 = original_str[0:6]
+    sub1 = original_str[7:18]
+    merged_str = sub1 + " " + sub2
     print(sub2)
     print(sub1)
     print(merged_str)
-
     #########################################
     # Do not delete the return statement
     return sub1, sub2, merged_str
